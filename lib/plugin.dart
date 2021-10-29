39ea44bc0a3343a6fb382cd70daa2561fd5b347c
GeghamHayrapetian <49583366+GeghamHayrapetian@users.noreply.github.com>
2021-10-29 12:55:05 +0400

    vv

--- lib/plugin.dart
+++ lib/plugin.dart
@@ -220,7 +220,7 @@ class FlutterMongoRealm {
       jsStream.listen((event) {
         var eventDetail = (event as CustomEvent).detail;
 
-        var map = json.decode(eventDetail as String);
+        var map = json.decode(eventDetail ?? '');
 
         if (map['_id'] is String == false) {
           map['_id'] = ObjectId.parse(map['_id']);
